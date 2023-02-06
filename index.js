595c4204a8c6fc203e2228a3e203d910d2660cab
clevessonmendonca <wanderbloks311@gmail.com>
2023-02-06 16:10:22 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Person } = require("./person");
+
+// require('./modules/path')
+//require('./modules/fs')
+require('./modules/http')
+
+const person = new Person('Cl')
+
+// console.log(person.sayMyName())
